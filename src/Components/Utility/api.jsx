b0bb9e76e07d36ae82cf62c9eeafcef84772ecb9
codewithevolg <oluwagbengaogundare@gmail.com>
2026-01-02 14:21:00 -0500

    teaching: use axios instead of fetch

--- src/Components/Utility/api.jsx
+++ src/Components/Utility/api.jsx
@@ -1,12 +1,9 @@
+import axios from "axios";
+
 const getRandomUser = async () => {
   const url = new URL("https://randomuser.me/api/");
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
-  }
-
-  const data = await response.json();
-  return data;
+  const response = await axios.get(url);
+  return response.data;
 };
 
 export default getRandomUser;
